e71d4bdd1fd45f19fcefbe720f20386f1b91d6a2
Kang-in Raon Kim <is.raonism@gmail.com>
2014-07-20 14:22:29 +0900

    very first draft of ajax call.

--- controller/libs/getDom.js
+++ controller/libs/getDom.js
@@ -3,7 +3,29 @@ $(document).ready(function() {
 
     var $target = $(event.target);
     if (!$target.closest('#ycs').length) {
-      $target.toggleClass(event.type == 'click' ? 'ycs-outline-clicked' : 'ycs-outline-hovered');      
+      $target.toggleClass(event.type == 'click' ? 'ycs-outline-clicked' : 'ycs-outline-hovered');
+
+      if(event.type == 'click') {
+        $.ajax({
+        type: 'POST',
+        data: {
+          'test': 'test',
+        },
+        url : 'http://bullback.cafe24.com/ajax/',
+        success: function(data) {
+          var msg = 'Request Status: ' + request.status + '\n';
+          msg += 'Request Errors: ' + error + '\n';
+          msg += 'Response Message: ' + request.responseText
+          alert(msg);
+        },
+        error: function(request, status, error) {
+          var msg = 'Request Status: ' + request.status + '\n';
+          msg += 'Request Errors: ' + error + '\n';
+          msg += 'Response Message: ' + request.responseText
+          alert(msg);
+        },
+      });
+      }      
     }
   });
 });
