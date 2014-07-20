356e5a6211a66b042600f9f2fd043d61235221c2
Kang-in Raon Kim <is.raonism@gmail.com>
2014-07-20 11:35:40 +0900

    make namespace 'ycs' by using less.

--- controller/libs/getDom.js
+++ controller/libs/getDom.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('body').bind('mouseover mouseout click', function(event) {
 
     var $target = $(event.target);
-    if (!$target.closest('#ycs-header').length) {
+    if (!$target.closest('#ycs').length) {
       $target.toggleClass(event.type == 'click' ? 'ycs-outline-clicked' : 'ycs-outline-hovered');      
     }
   });
