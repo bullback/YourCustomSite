adb86dd7a5a45b07db90425b0a5c25dd9bde62f8
Kang-in Raon Kim <is.raonism@gmail.com>
2014-07-22 05:53:44 +0900

    display handler when target DOM clicked.

--- controller/controller.js
+++ controller/controller.js
@@ -4,16 +4,16 @@
 
 	var attr, css, cls;
 	attr = {
-		'id': 'ycs',
+		'id': 'ycs-wrapper',
 	};
 
 	css = {
 		'position': 'fixed',
-    'top': '0',
-    'left': '0',
-    'width': '100%',
-    'height': '100px',
-    'z-index': '10000',
+	    'top': '0',
+	    'left': '0',
+	    'width': '100%',
+	    'height': '100px',
+	    'z-index': '10000',
 	};
 
 	cls = [
@@ -22,7 +22,7 @@
 
 	var wrapper = putDom('body', 'div', attr, css, cls)
 	var content = [
-    "<div class=\"alert alert-info alert-dismissible\" role=\"alert\" style=\"height:50px;\">",
+	    "<div class=\"alert alert-info alert-dismissible\" role=\"alert\" style=\"height:50px;\">",
 		"  <button type=\"button\" class=\"close\" data-dismiss=\"alert\">",
 		"    <span aria-hidden=\"true\">&times;</span>",
 		"    <span class=\"sr-only\">Close</span>",
@@ -30,10 +30,52 @@
 		"  <p class=\"pull-left\" style=\"margin-top: 1px;\"><strong>사이트 레이아웃을 원하는 모습으로 바꿔보세요.</strong> 이미 <strong>1</strong>개의 사이트 레이아웃이 제작되어 있습니다.</p>",
 		"  <p class=\"pull-right\" style=\"margin-top: 1px;\"><i class=\"fa fa-search\"></i> 레이아웃 찾아보기&nbsp;&nbsp;</p>",
 		"  <p class=\"pull-right\" style=\"margin-top: 1px;\"><i class=\"fa fa-file-text-o\"></i> 레이아웃 제작하기&nbsp;&nbsp;</p>",
-    "</div>",
-  ].join('\n');
+	    "</div>",
+	].join('\n');
 
 	wrapper.append(content);
+
+	attr = {
+		'id': 'ycs-handler',
+	};
+
+	css = {
+		'position': 'absolute',
+	    'top': '0',
+	    'left': '0',
+	    'width': '0',
+	    'height': '0',
+	    'z-index': '10000',
+	};
+
+	cls = [
+		'ycs',
+	];
+
+	var handler = putDom('body', 'div', attr, css, cls)
+
+	var content = [
+		"<div class=\"modal fade\" id=\"handler\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"handler\" aria-hidden=\"true\">",
+		"  <div class=\"modal-dialog modal-sm\">",
+		"    <div class=\"modal-content\">",
+		"      <div class=\"modal-header\">",
+		"        <button type=\"button\" class=\"close\" data-dismiss=\"modal\"><span aria-hidden=\"true\">&times;</span><span class=\"sr-only\">Close</span></button>",
+		"        <h4 class=\"modal-title\" id=\"handlerLabel\">Modal title</h4>",
+		"      </div>",
+		"      <div class=\"modal-body\">",
+		"        ...",
+		"      </div>",
+		"      <div class=\"modal-footer\">",
+		"        <button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button>",
+		"        <button type=\"button\" class=\"btn btn-primary\">Save changes</button>",
+		"      </div>",
+		"    </div>",
+		"  </div>",
+		"</div>",
+	].join('\n');
+	
+	handler.append(content);
+	
 
 	attr = {
 
--- controller/libs/getDom.js
+++ controller/libs/getDom.js
@@ -2,30 +2,12 @@ $(document).ready(function() {
   $('body').bind('mouseover mouseout click', function(event) {
 
     var $target = $(event.target);
-    if (!$target.closest('#ycs').length) {
+    if (! ($target.closest('#ycs-wrapper').length || $target.closest('#ycs-handler').length)) {
       $target.toggleClass(event.type == 'click' ? 'ycs-outline-clicked' : 'ycs-outline-hovered');
 
       if(event.type == 'click') {
-        $.ajax({
-        type: 'POST',
-        data: {
-          'test': 'test',
-        },
-        url : 'http://bullback.cafe24.com/ajax/',
-        success: function(data) {
-          var msg = 'Request Status: ' + request.status + '\n';
-          msg += 'Request Errors: ' + error + '\n';
-          msg += 'Response Message: ' + request.responseText
-          alert(msg);
-        },
-        error: function(request, status, error) {
-          var msg = 'Request Status: ' + request.status + '\n';
-          msg += 'Request Errors: ' + error + '\n';
-          msg += 'Response Message: ' + request.responseText
-          alert(msg);
-        },
-      });
-      }      
+        $('#ycs-handler #handler').modal('show');
+      }
     }
   });
 });
