1de90bf9ac28d342d366022f7d6e7babe76f68c6
Kang-in Raon Kim <is.raonism@gmail.com>
2014-07-22 17:15:18 +0900

    add cancel button. add functionality to locate, delete button.

--- controller/libs/getDom.js
+++ controller/libs/getDom.js
@@ -6,8 +6,41 @@ $(document).ready(function() {
       $target.toggleClass(event.type == 'click' ? 'ycs-outline-clicked' : 'ycs-outline-hovered');
 
       if(event.type == 'click') {
+        $target.addClass('ycs-clicked');
         $('#ycs-handler #handler').modal('show');
+        /*
+        $.ajax({
+          type: 'POST',
+          data: {
+            'test': 'test',
+          },
+          url : 'http://bullback.cafe24.com/ajax/',
+          success: function(data) {
+            var msg = 'Request Status: ' + request.status + '\n';
+            msg += 'Request Errors: ' + error + '\n';
+            msg += 'Response Message: ' + request.responseText
+            alert(msg);
+          },
+          error: function(request, status, error) {
+            var msg = 'Request Status: ' + request.status + '\n';
+            msg += 'Request Errors: ' + error + '\n';
+            msg += 'Response Message: ' + request.responseText
+            alert(msg);
+          },
+        });
+        */
       }
+      
     }
+  });
+
+  $('#ycs-handler #handler #locate').bind('click', function(event) {
+    console.log('locate');
+    $('.ycs-clicked').draggable();
+  });
+
+  $('#ycs-handler #handler #delete').bind('click', function(event) {
+    console.log('delete');
+    $('.ycs-clicked').hide();
   });
 });
